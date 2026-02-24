baba42e20d779e643625453eb9ddc999cfcea0fc
yanaaa-it <yaninalikemiki@gmail.com>
2026-02-24 12:37:25 +0300

    new

--- task01/logic.cpp
+++ task01/logic.cpp
@@ -1,13 +1,11 @@
 #include"logic.h"
 string even_odd_number(int array[DEFAULT_SIZE], int length) {
 	int sum_even_number = 0;
-	int sum_odd_number = 0;
 
 	for (int index = 0; index < length; index++) {
 		sum_even_number += array[index] % 2 ? 1 : 0;
-		sum_odd_number += array[index] % 2 ? 0 : 1;
 	}
-	string msg = to_string(sum_even_number) +"\n odd numbers:" + to_string(sum_odd_number);
+	string msg = to_string(sum_even_number) +"\n odd numbers:" + to_string(length-sum_even_number);
 
 	return msg;
 }
